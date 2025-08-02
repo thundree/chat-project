b3ceeed600cc8bc67e2f8ffcc14abab78a2cf1bd
thundree <129085761+thundree@users.noreply.github.com>
2025-08-02 11:28:32 -0300

    Updates

---
+++ assets/AlertModal-ClCbrFCF.js
@@ -0,0 +1,1 @@
+import{j as e,S as l}from"./index-B-U-4uMl.js";function c({show:t,onClose:r,children:s,okText:a="OK"}){return e.jsx(l,{show:t,onClose:r,cancelText:a,children:e.jsx("div",{className:"flex flex-col items-center justify-center",children:e.jsx("div",{className:"flex-1 text-gray-800 dark:text-white mb-4",children:s})})})}export{c as default};
